2628eadb3ca3f4e4c5270a5718c29f69502d4bc3
wise0world <dimaglazyrin10@gmail.com>
2025-05-18 16:20:07 +0700

    Add files via upload

---
+++ scirpt.js
@@ -0,0 +1,28 @@
+const btn = document.getElementById("btn-Clan");
+btn.addEventListener('click', () => {
+    alert("Вы успешно получили ссылку на дискорд кананал клана .QLS https://discord.gg/QNb8nAnZ ");
+})
+
+let audio = new Audio('cute-triangle.mp3');
+function play_sound() {
+    audio.play();
+    console.log(1)
+}
+
+const btngg = document.getElementById("btn-build")
+btngg.addEventListener("click",play_sound)
+
+const btngg1 = document.getElementById("btn-Hazmat")
+btngg1.addEventListener("click",play_sound)
+
+const btngg12 = document.getElementById("btn-Animal")
+btngg12.addEventListener("click",play_sound)
+
+const btngg123 = document.getElementById("btn-Vehicle")
+btngg123.addEventListener("click",play_sound)
+
+const btngg1234 = document.getElementById("btn-Clan")
+btngg1234.addEventListener("click",play_sound)
+
+const btngg12345 = document.getElementById("btn-Rust")
+btngg12345.addEventListener("click",play_sound)
